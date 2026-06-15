5f04f6df9447d0bb173957f477feb37f59e92cb7
sioodmy <hello@sioodmy.dev>
2026-06-15 19:41:22 +0200

    feat: use nix for latex docs

--- flake.nix
+++ flake.nix
@@ -60,20 +60,48 @@
           '';
         };
 
+        texlive = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-small
+            collection-langpolish
+            cm-super
+            geometry
+            hyperref
+            float
+            caption
+            booktabs
+            enumitem
+            listings
+            xcolor
+            ;
+        };
+
         docs = pkgs.stdenv.mkDerivation {
           pname = "sor-docs";
           version = "1.0.0";
           src = ./.;
 
-          nativeBuildInputs = [ pkgs.doxygen ];
+          nativeBuildInputs = [
+            pkgs.doxygen
+            texlive
+          ];
 
           buildPhase = ''
+            export HOME=$TMPDIR
+
             doxygen Doxyfile
+
+            cd docs
+            pdflatex -interaction=nonstopmode main.tex
+            pdflatex -interaction=nonstopmode main.tex
+            cd ..
           '';
 
           installPhase = ''
-            mkdir -p $out
-            cp -r docs_out/html/* $out/
+            mkdir -p $out/doxygen
+            cp -r docs_out/html/* $out/doxygen/
+
+            cp docs/main.pdf $out/docs.pdf
           '';
         };
 
@@ -98,6 +126,7 @@
             pkgs.gcc
             pkgs.valgrind
             pkgs.doxygen
+            texlive
             pkgs.gtest
             pythonEnv
           ];
